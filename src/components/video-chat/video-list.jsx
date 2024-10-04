73ae6941f5172406fb871c98ffbc06f5e08daa39
zephyr <625649687@qq.com>
2024-10-04 11:18:18 +0800

    fix

--- src/components/video-chat/video-list.jsx
+++ src/components/video-chat/video-list.jsx
@@ -325,7 +325,14 @@ const VideoList = () => {
           {state.listType === 'list' && (
             <div className="video-progress">
               {Array.from({ length: 20 }, () => Math.floor(Math.random() * 100) + 1).map((position, i) => (
-                <Tooltip key={i} title={<img src="1.png" style={{ width: '10rem', height: '8rem' }} />} placement="top">
+                <Tooltip
+                  classes={{
+                    tooltip: 'video-keyClip-thumb',
+                  }}
+                  key={i}
+                  title={<img src="1.png" style={{ width: '10rem', height: '8rem' }} />}
+                  placement="top"
+                >
                   <div className="progress-keyClip" style={{ left: `${position}%` }} onClick={() => playerVideo(item.videoUrl, position / 100)} />
                 </Tooltip>
               ))}
